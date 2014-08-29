5eb1e23191a98627d8bb56657d210fbd09032460
pranayd-cuelogic <pranay.dubey@cuelogic.co.in>
2014-08-29 22:48:21 +0530

    Verify each member of ng-pass to be function

--- ng-next.js
+++ ng-next.js
@@ -6,14 +6,31 @@ angular.module('next',[])
             var arr = $scope[pass];
             var event = $attrs.ngPassEvent || 'click';
             if(!arr.length) throw new Error('should be an array');
+            var len = arr.length;
+            /*
+                @swagger this will check for the presence of each element as a
+                function inside the array pass and throw error if any of them
+                do not exist or is not a function
+            */
+            function walkThrough(arr,index) {
+                if(index === arr.length) return;
+                var curr = $scope[arr[index]];
+                if(typeof curr !== 'function') {
+                    var memberType = typeof curr;
+                    throw new Error(curr + ' should be a function but instead found ' + memberType);  
+                } 
+                index += 1;
+                walkThrough(arr,index);
+            }
+            walkThrough(arr,index);
+            
             $scope.next = function(err) {
                 if(err)  {
-
                     //to be call if an error occured
                     dispose(err); 
                     return;
                 };
-		if($scope.nxMethod) call();
+		        if($scope.nxMethod) call();
             };
 
             var call = function() {
@@ -21,14 +38,14 @@ angular.module('next',[])
                 if(curr && $scope[curr]) { 
                     $scope.nxCallingIndex += 1;
                     $scope[curr]();
-		    return;
-                };
-		$scope.nxMethod = false;           
+		            return;
+                }
+		        $scope.nxMethod = false;           
             };
 
             var dispose = function(err) {
                 var errHandler = $attrs.ngPassErr;
-		$scope.nxMethod = false;
+		        $scope.nxMethod = false;
                 if($scope[errHandler]) {
                     $scope[errHandler](err);
                 };
@@ -39,9 +56,9 @@ angular.module('next',[])
                 var arr = $attrs.ngPass;
                 $scope.nxCallingArr = $scope[arr];
                 if($scope.nxCallingArr && $scope.nxCallingArr.length) {
-		    $scope.nxMethod = true;
-		    call();
-		}; 
+		          $scope.nxMethod = true;
+		          call();
+		        }; 
             });
         }
     }
