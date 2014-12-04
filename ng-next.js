f790bbb27f1ae7fc7ce83951b6af38833b8b03d3
pranayd-cuelogic <pranay.dubey@cuelogic.co.in>
2014-12-05 03:48:45 +0530

    Finally some work done from the airport.. yeyyy :D

--- ng-next.js
+++ ng-next.js
@@ -1,85 +1,108 @@
 angular.module('next',[])
 .directive('ngPass',function() {
-    return {
-        link : function($scope,$elem,$attrs) {
-            var pass = $attrs.ngPass;
-            var arr = extractArray(pass);
-            var event = $attrs.ngPassEvent || 'click';
-            var len = arr.length;
-            /*
-                @swagger this will check for the presence of each element as a
-                function inside the array pass and throw error if any of them
-                do not exist or is not a function
-            */
-            function walkThrough(arr,index) {
-                if(index === arr.length) return;
-                var curr = $scope[arr[index]];
-                if(typeof curr !== 'function') {
-                    var memberType = typeof curr;
-                    throw new Error(curr + ' should be a function but instead found ' + memberType);  
-                } 
-                index += 1;
-                walkThrough(arr,index);
-            }
-            walkThrough(arr,0);
-            /*
-                @swagger method that returns the actual array from the argument
-                @args can be passed an array name that exist in the scope OR an array itself that has list of functions
-            */
-            function extractArray(arr) {
-                var arr = arr;
-                if (!arr) throw new Error('Argument passed in at ng-pass cannot be undefined');
+  return {
+    link : function($scope,$elem,$attrs) {
 
-                arr = $scope.$eval(arr);
-                
-                if (!arr) throw new Error('Argument passed in at ng-pass does not exist in the current scope');
+      var pass = $attrs.ngPass;
+      var arr = extractArray(pass);
+      var event = $attrs.ngPassEvent || 'click';
+      var len = arr.length;
+      
+      /*
+        the following will loop through each element of the array,
+        throws error if any of them is'nt a function
+      */
+      function walkThrough(arr,index) {
 
-                var isArray = Array.isArray(arr);
+        if(index === arr.length) return;
+        var curr = $scope[arr[index]];
+        if(typeof curr !== 'function') {
+            var memberType = typeof curr;
+            throw new Error(curr + ' should be a function but instead found ' + memberType);  
+        } 
+        index += 1;
+        walkThrough(arr,index);
 
-                if(!isArray) throw new Error('Argument passed in at ng-pass must be an array');
+      }
 
-                if (!arr.length) throw new Error('Argument passed in at ng-pass cannot be an empty array');
+      // immediately invoke the walkthrough on the element on directive load
+      walkThrough(arr,0);
 
-                return arr;
+      /*
+        @swagger method that returns the actual array from the argument
+        @args can be passed an array name that exist in the scope OR an array itself that has list of functions
+      */
+      function extractArray(arr) {
+        
+        var arr = arr;
+        if (!arr) throw new Error('Argument passed in at ng-pass cannot be undefined');
 
-            }
+        arr = $scope.$eval(arr);
+        
+        if (!arr) throw new Error('Argument passed in at ng-pass does not exist in the current scope');
 
-            $scope.next = function(err) {
-                if(err)  {
-                    //to be call if an error occured
-                    dispose(err); 
-                    return;
-                };
-		        if($scope.nxMethod) call();
-            };
+        var isArray = Array.isArray(arr);
 
-            var call = function() {
-                var curr = $scope.nxCallingArr[$scope.nxCallingIndex];
-                if(curr && $scope[curr]) { 
-                    $scope.nxCallingIndex += 1;
-                    $scope[curr]();
-		            return;
-                }
-		        $scope.nxMethod = false;           
-            };
+        if(!isArray) throw new Error('Argument passed in at ng-pass must be an array');
 
-            var dispose = function(err) {
-                var errHandler = $attrs.ngPassErr;
-		        $scope.nxMethod = false;
-                if($scope[errHandler]) {
-                    $scope[errHandler](err);
-                };
-            };
-            
-            $elem.bind(event,function() {
-                $scope.nxCallingIndex = 0;
-                var arr = extractArray($attrs.ngPass);
-                $scope.nxCallingArr = arr;
-                if($scope.nxCallingArr && $scope.nxCallingArr.length) {
-		          $scope.nxMethod = true;
-		          call();
-		        }; 
-            });
+        if (!arr.length) throw new Error('Argument passed in at ng-pass cannot be an empty array');
+
+        return arr;
+
+      }
+
+      // this is the function that will be called from the controller
+      $scope.next = function(err) {
+
+        if(err)  {
+          //to be call if an error occured
+          dispose(err); 
+          return;
+        };
+        if($scope.nxMethod) call();
+
+      };
+
+      // this will call each function from the $scope array
+      var call = function() {
+
+        var curr = $scope.nxCallingArr[$scope.nxCallingIndex];
+        if(curr && $scope[curr]) { 
+          $scope.nxCallingIndex += 1;
+          $scope[curr]();
+          return;
         }
+        $scope.nxMethod = false;
+
+      };
+
+      // if there is any error passed in call the dispose and exit calling array functions
+      var dispose = function(err) {
+        
+        var errHandler = $attrs.ngPassErr;
+        $scope.nxMethod = false;
+        if($scope[errHandler]) {
+            $scope[errHandler](err);
+        };
+
+      };
+      
+      /*
+          @params event -- the HTML event passed in from the directive argument itself
+              defaults to false
+      */
+      $elem.bind(event,function() {
+        
+        $scope.nxCallingIndex = 0;
+        var arr = extractArray($attrs.ngPass);
+        $scope.nxCallingArr = arr;
+        if($scope.nxCallingArr && $scope.nxCallingArr.length) {
+          $scope.nxMethod = true;
+          call();
+        };
+
+      });
+
     }
+  }
 });
